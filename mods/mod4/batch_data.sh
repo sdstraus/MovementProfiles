fe7cb804dbcb55027bbebb56883c9c704d432428
Samantha Straus <straus@zoology.ubc.ca>
2020-09-17 12:04:03 -0700

    Add files via upload

---
+++ mods/mod4/batch_data.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#SBATCH --account=def-germainr   # replace this with your own account
+#SBATCH --job-name=mod4_2    		# Job name
+#
+#SBATCH --ntasks=4               # number of processes
+#SBATCH --mem-per-cpu=7000M      # memory; default unit is megabytes
+#SBATCH --time=7-00:00           # time (DD-HH:MM)
+#SBATCH --mail-user=straus@zoology.ubc.ca # Send email updates to you or someone else
+#SBATCH --mail-type=ALL          # send an email in all cases (job started, job ended, job aborted)
+
+echo "Running on hostname `hostname`"
+
+cd $SLURM_SUBMIT_DIR
+echo "Working directory is `pwd`"
+echo "Starting R at `date`."
+
+module load gcc
+module load r/3.5.0
+R --vanilla 
+Rscript mod4_2.R 
+
+echo "Finished R at `date`."
---
+++ mods/mod4/mod4_2.R
@@ -0,0 +1,60 @@
+library(brms)
+library(stringr)
+library(dplyr)
+library(ape)
+
+### load traits
+######## googlesheets sucks now :(
+
+traits_clean <- read.csv("../../data/clean_traits.csv")
+
+## load phylogeny
+
+one_phylo <- readRDS("../../data/final_phylo.rds")
+
+one_phylo
+
+all_class <- read.csv("../../data/classes.csv")
+
+traits_phylo <- traits_clean %>% 
+  dplyr::filter(number_traits_completed ==4) %>% 
+  left_join(all_class, by = c("scientific_name.x" = "scientific_name")) %>% 
+  filter(!(class %in% "Insecta")) %>% 
+  #bind_rows(shark_1) %>% 
+  dplyr::filter(str_replace(scientific_name.x, " ", "_") %in% one_phylo$tip.label) %>% 
+  select(scientific_name.x, dispersal_km, Migration_km, mean.hra.m2, Mass_kg, class) %>% 
+  mutate(scientific_name.x = str_replace(scientific_name.x, " ", "_")) %>% 
+  mutate(dispersal_km = dispersal_km + 0.0001) %>% 
+  mutate(mean.hra.m2 = as.numeric(as.character(mean.hra.m2))) %>% 
+  mutate(Migration_km = as.numeric(as.character(Migration_km))) %>% 
+  mutate(class = as.character(class))
+
+
+A <- ape::vcv.phylo(one_phylo)
+
+bf_dispersal <- bf(dispersal_km ~ 1+ (1|scientific_name.x)) + Gamma(link = "log")
+bf_home_range <- bf(mean.hra.m2 ~ 1+ (1|scientific_name.x)) + Gamma(link = "log")
+bf_migration <- bf(Migration_km ~ 1+ (1|scientific_name.x)) + hurdle_gamma(link = 'log')
+
+mod4_prior <- get_prior(bf_dispersal + bf_home_range + bf_migration + set_rescor(FALSE), cov_ranef = list(scientific_name.x = A),
+                        data = traits_phylo)
+
+mod4_prior$prior[mod4_prior$prior == "gamma(0.01, 0.01)"] <- "gamma(0.1, 0.1)"
+
+mod4_prior$prior[mod4_prior$class == "Intercept"][2] <- "normal(2,20)"
+
+mod4_prior$prior[mod4_prior$class == "Intercept"][3] <- "normal(11,20)"
+
+mod4_prior$prior[mod4_prior$class == "Intercept"][4] <- "normal(1,20)"
+
+mod4_prior$prior[mod4_prior$prior == "student_t(3, 0, 10)"] <- "student_t(3, 0, 5)"
+
+
+mod4 <- brm(
+  bf_dispersal + bf_home_range + bf_migration + set_rescor(FALSE), cov_ranef = list(scientific_name.x = A),
+  data = traits_phylo, chains = 4, cores = 4, iter =10000,
+  control = list(adapt_delta = 0.995, max_treedepth = 15),
+  prior = mod4_prior
+)
+
+saveRDS(mod4, "mod4_better_model_des.rds")
