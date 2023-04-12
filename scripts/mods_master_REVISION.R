90eee03575c87aa994f8431075410891f978e627
Laura Melissa Guzman <melissa@DTS-H2WHN17YQ6NY.dts.usc.edu>
2023-04-12 13:07:03 -0700

    added priors and changed the distribution, most of the trophic models are down to 1 or no divergent transitions

--- scripts/mods_master_REVISION.R
+++ scripts/mods_master_REVISION.R
@@ -14,42 +14,85 @@ levels(traits_phylo$diet_broadest_cat)
 ##### Trophic Guild #####
 
 # random slope, no year
-bf_dispersal_trophic_noyear <- bf(dispersal_km ~ log(Mass_kg) + (log(Mass_kg)|p|diet_broadest_cat)) + Gamma(link = "log")
-bf_home_range_trophic_noyear <- bf(hr.radius ~ log(Mass_kg)+ (log(Mass_kg)|p|diet_broadest_cat)) + Gamma(link = "log")
-bf_migration_trophic_noyear <- bf(Migration_km ~ log(Mass_kg) + (log(Mass_kg)|p|diet_broadest_cat)) + hurdle_gamma(link = 'log')
+bf_dispersal_trophic_noyear <- bf(dispersal_km ~ log(Mass_kg) + (log(Mass_kg)|diet_broadest_cat)) + lognormal(link = "identity")
+bf_home_range_trophic_noyear <- bf(hr.radius ~ log(Mass_kg)+ (log(Mass_kg)|diet_broadest_cat)) + lognormal(link = "identity")
+bf_migration_trophic_noyear <- bf(Migration_km ~ log(Mass_kg) + (log(Mass_kg)|diet_broadest_cat)) + hurdle_lognormal(link = 'identity')
 
+trophic_noyear_prior <- prior(normal(0,5),class = 'b', coef = logMass_kg) +
+  prior(student_t(4, 0, 3), class = 'Intercept') +
+  prior(student_t(6, 0, 2.9), class = 'sd') +
+  prior(student_t(4, 0, 2.9), class = 'sigma')
 
+## this model is good
 trophic.disp_noyear <- brm(bf_dispersal_trophic_noyear, data = traits_phylo, 
-                   control = list(adapt_delta = 0.995, max_treedepth=20), iter = 4000, cores = 4)
+                   control = list(adapt_delta = 0.995, max_treedepth=20), iter = 2000, cores = 4,
+                   prior = trophic_noyear_prior)
 
+## this model is good
 trophic.for_noyear <- brm(bf_home_range_trophic_noyear, data = traits_phylo, 
-                  control = list(adapt_delta = 0.99, max_treedepth=20), iter = 4000, cores = 4)
+                  control = list(adapt_delta = 0.995, max_treedepth=20), iter = 2000, cores = 4,
+                  prior = trophic_noyear_prior)
 
+trophic_noyear_prior_mig <- prior(normal(0,4),class = 'b', coef = logMass_kg) +
+  prior(student_t(3, 0, 3), class = 'Intercept') +
+  prior(student_t(6, 0, 2.9), class = 'sd') +
+  prior(student_t(3, 0, 2.9), class = 'sigma')
+
+
+## 1 divergent transitions, see if we can fix that 
 trophic.mig_noyear <- brm(bf_migration_trophic_noyear, data = traits_phylo, 
-                  control = list(adapt_delta = 0.95, max_treedepth=15), iter = 3000, cores = 4)
+                  control = list(adapt_delta = 0.995, max_treedepth=20), iter = 3000, cores = 4, 
+                  prior = trophic_noyear_prior_mig)
 
 
-saveRDS(trophic.disp_noyear, "../mods/mod4.1/trophic.disp_noyear.rds")
-saveRDS(trophic.for_noyear, "../mods/mod4.1/trophic.for_noyear.rds")
-saveRDS(trophic.mig_noyear, "../mods/mod4.1/trophic.mig_noyear.rds")
+saveRDS(trophic.disp_noyear, "mods/mod4.1/trophic.disp_noyear.rds")
+saveRDS(trophic.for_noyear, "mods/mod4.1/trophic.for_noyear.rds")
+saveRDS(trophic.mig_noyear, "mods/mod4.1/trophic.mig_noyear.rds")
 
 
 # random slope, with year
-bf_dispersal_trophic_withyear <- bf(dispersal_km ~ log(Mass_kg) + (log(Mass_kg)|p|diet_broadest_cat) + dispersal_year) + Gamma(link = "log")
-bf_home_range_trophic_withyear <- bf(hr.radius ~ log(Mass_kg)+ (log(Mass_kg)|p|diet_broadest_cat) + foraging_year) + Gamma(link = "log")
-bf_migration_trophic_withyear <- bf(Migration_km ~ log(Mass_kg) + (log(Mass_kg)|p|diet_broadest_cat) + migration_year) + hurdle_gamma(link = 'log')
+bf_dispersal_trophic_withyear <- bf(dispersal_km ~ log(Mass_kg) + (log(Mass_kg)|diet_broadest_cat) + dispersal_year) + lognormal(link = "identity")
+bf_home_range_trophic_withyear <- bf(hr.radius ~ log(Mass_kg)+ (log(Mass_kg)|diet_broadest_cat) + foraging_year) + lognormal(link = "identity")
+bf_migration_trophic_withyear <- bf(Migration_km ~ log(Mass_kg) + (log(Mass_kg)|diet_broadest_cat) + migration_year) + hurdle_lognormal(link = 'identity')
 
 
+trophic_year_prior_disp <- prior(normal(0,5),class = 'b', coef = logMass_kg) +
+  prior(normal(0,5),class = 'b', coef = dispersal_year) +
+  prior(student_t(5, 0, 3), class = 'Intercept') +
+  prior(student_t(6, 0, 2.9), class = 'sd') +
+  prior(student_t(5, 0, 2.9), class = 'sigma')
+
+## 1 divergent transition 
 trophic.disp_withyear <- brm(bf_dispersal_trophic_withyear, data = traits_phylo, 
-                          control = list(adapt_delta = 0.995, max_treedepth=20), iter = 4000, cores = 4)
+                          control = list(adapt_delta = 0.995, max_treedepth=20), iter = 2000, cores = 4,
+                          prior = trophic_year_prior_disp)
 
+trophic_year_prior_for <- prior(normal(0,5),class = 'b', coef = logMass_kg) +
+  prior(normal(0,5),class = 'b', coef = foraging_year) +
+  prior(student_t(5, 0, 3), class = 'Intercept') +
+  prior(student_t(6, 0, 2.9), class = 'sd') +
+  prior(student_t(5, 0, 2.9), class = 'sigma')
+
+# 1 divergent transitions
 trophic.for_withyear <- brm(bf_home_range_trophic_withyear, data = traits_phylo, 
-                         control = list(adapt_delta = 0.99, max_treedepth=20), iter = 4000, cores = 4)
+                         control = list(adapt_delta = 0.995, max_treedepth=20), iter = 2000, cores = 4,
+                         prior = trophic_year_prior_for)
+
+
+trophic_year_prior_mig <- prior(normal(0,5),class = 'b', coef = logMass_kg) +
+  prior(normal(0,5),class = 'b', coef = migration_year) +
+  prior(student_t(2, 0, 3), class = 'Intercept') +
+  prior(student_t(6, 0, 2.9), class = 'sd') +
+  prior(student_t(5, 0, 2.9), class = 'sigma')
+
 
 trophic.mig_withyear <- brm(bf_migration_trophic_withyear, data = traits_phylo, 
-                         control = list(adapt_delta = 0.95, max_treedepth=15), iter = 3000, cores = 4)
+                         control = list(adapt_delta = 0.95, max_treedepth=15), iter = 5000, cores = 4,
+                         prior = trophic_year_prior_mig)
 
-
+saveRDS(trophic.disp_withyear, "mods/mod4.1/trophic.disp_withyear.rds")
+saveRDS(trophic.for_withyear, "mods/mod4.1/trophic.for_withyear.rds")
+saveRDS(trophic.mig_withyear, "mods/mod4.1/trophic.mig_withyear.rds")
 
 ##### Class #####
 
